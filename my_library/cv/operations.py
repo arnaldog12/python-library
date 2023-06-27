38b439d9d70577b9a2a8351608b82662a91d0ffa
Arnaldo Gualberto <arnaldo.g12@gmail.com>
2023-06-27 16:07:53 -0300

    fix: crop operation

--- my_library/cv/operations.py
+++ my_library/cv/operations.py
@@ -14,7 +14,7 @@ def bright(image: NDArray[np.uint8], value: int) -> NDArray[np.uint8]:
     Returns:
         NDArray[np.uint8]: same shape of image
     """
-    return np.clip(image + value, 0, 255)
+    return np.clip(image.astype(np.int16) + value, 0, 255).astype(np.uint8)
 
 
 def crop(image: NDArray[np.uint8], top: int, left: int, bottom: int, right: int) -> NDArray[np.uint8]:
@@ -30,4 +30,7 @@ def crop(image: NDArray[np.uint8], top: int, left: int, bottom: int, right: int)
     Returns:
         NDArray[np.uint8]: image cropped by params.
     """
+    if top < 0 or left < 0 or bottom > image.shape[0] or right > image.shape[1]:
+        raise ValueError("Invalid crop parameters")
+
     return image[top:bottom, left:right]
